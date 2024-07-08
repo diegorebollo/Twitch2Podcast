09a373b094e9fd445078f2c0100f4bd22d376306
Diego Rebollo <diego@drebollo.com>
2024-07-08 10:44:18 +0200

    more sqlite stuff

--- cmd/Twitch2Podcasts/main.go
+++ cmd/Twitch2Podcasts/main.go
@@ -15,47 +15,6 @@ func main() {
 
 	dbmanager.InitDb(dbmanager.ConnectDb())
 
-	dbTest := dbmanager.ConnectDb()
-
-	go dbmanager.SearchChannel("chiclanafriends", dbTest)
-	go dbmanager.SearchChannel("pokimane", dbTest)
-	go dbmanager.SearchChannel("ibai", dbTest)
-	go dbmanager.SearchChannel("illojuan", dbTest)
-	go dbmanager.SearchChannel("orslok", dbTest)
-	go dbmanager.SearchChannel("el_yuste", dbTest)
-	go dbmanager.SearchChannel("elxokas", dbTest)
-	go dbmanager.SearchChannel("ikurotime", dbTest)
-	go dbmanager.SearchChannel("alexelcapo", dbTest)
-	go dbmanager.SearchChannel("chicocartera", dbTest)
-	go dbmanager.SearchChannel("jujalag", dbTest)
-	go dbmanager.SearchChannel("knekro", dbTest)
-	go dbmanager.SearchChannel("eurogamer_es", dbTest)
-	go dbmanager.SearchChannel("viviendoenlacalle", dbTest)
-	go dbmanager.SearchChannel("rubius", dbTest)
-	go dbmanager.SearchChannel("auronplay", dbTest)
-	go dbmanager.SearchChannel("littleragergirl", dbTest)
-	go dbmanager.SearchChannel("anujbost", dbTest)
-	go dbmanager.SearchChannel("5ro4", dbTest)
-	go dbmanager.SearchChannel("japanwolf", dbTest)
-	go dbmanager.SearchChannel("llunaclark", dbTest)
-	go dbmanager.SearchChannel("pazos64", dbTest)
-	go dbmanager.SearchChannel("pandarina", dbTest)
-	go dbmanager.SearchChannel("rickyedit", dbTest)
-	go dbmanager.SearchChannel("elrichmc", dbTest)
-	go dbmanager.SearchChannel("kaicenat", dbTest)
-	go dbmanager.SearchChannel("jynxzi", dbTest)
-	go dbmanager.SearchChannel("tarik", dbTest)
-	go dbmanager.SearchChannel("diariomarca", dbTest)
-	go dbmanager.SearchChannel("caseoh_", dbTest)
-	go dbmanager.SearchChannel("gamesdonequick", dbTest)
-	go dbmanager.SearchChannel("cazetv_", dbTest)
-	go dbmanager.SearchChannel("squeezie", dbTest)
-	go dbmanager.SearchChannel("ewc_gold", dbTest)
-	go dbmanager.SearchChannel("loud_coringa", dbTest)
-	go dbmanager.SearchChannel("xqc", dbTest)
-	go dbmanager.SearchChannel("mixwell", dbTest)
-	go dbmanager.SearchChannel("valorant_americas", dbTest)
-
 	// go jobs.UpdateVods()
 
 	staticHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("static/")))
@@ -72,6 +31,8 @@ func main() {
 			return
 		}
 
+		db := dbmanager.ConnectDb()
+
 		channel := strings.ToLower(req.PostFormValue("channel"))
 
 		if strings.Contains(channel, "twitch.tv/") {
@@ -87,7 +48,7 @@ func main() {
 		var search models.Search
 
 		if len(channel) > 1 && len(channel) <= 25 {
-			search = dbmanager.SearchChannel(channel, dbmanager.ConnectDb())
+			search = dbmanager.SearchChannel(channel, db)
 		}
 
 		if search.Channel == nil {
